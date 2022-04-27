66079affe36512a1ef27f4c8e78372a1fe25fd71
mpenajoia <penajoia.marco@gmail.com>
2022-04-27 14:08:02 -0700

    .on to .once

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useReducer } from 'react';
+import Gun from 'gun';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// initialize Gun
+const gun = Gun({
+  peers: [
+    'http://localhost:4000/gun'
+  ]
+});
+
+const initialState = {
+  messages: []
 }
 
-export default App;
+function reducer(state, message) {
+  return {
+    messages: [message, ...state.messages]
+  }
+};
+
+export default function App() {
+  const [formState, setFormState] = useState({
+    name:'', message: ''
+  });
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const messages = gun.get('messages');
+    messages.map().once(m => {
+      dispatch({
+        name: m.name,
+        message: m.message,
+        // createdAt: m.createdAt
+      });
+    });
+  }, []);
+  
+  function onChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  };
+
+  function saveMessage() {
+    const messages = gun.get('messages');
+    messages.set({
+      name: formState.name,
+      message: formState.message,
+      // createdAt: Date.now()
+    });
+    setFormState({
+      name:'', message: ''
+    });
+  };
+  function clearMessage() {
+    const messages = gun.get('messages');
+    console.log(messages)
+    messages.put(null)
+  }
+
+
+  return (
+    <div>
+      <input
+        onChange={onChange}
+        placeholder='Name'
+        name="name"
+        value={formState.name}
+      />
+      <input
+        onChange={onChange}
+        placeholder='Message'
+        name="message"
+        value={formState.message}
+      />
+      <button onClick={saveMessage}>Send Message</button>
+      <button onClick={clearMessage}>Clear Messages</button>
+      {
+        state.messages.map((message, key) => (
+          <div key={key}>
+            <h2>{message.message}</h2>
+            <h3>Form: {message.name}</h3>
+            {/* <p>Date: {message.createdAt}</p> */}
+          </div>
+        ))
+      }
+    </div>
+  );
+
+};
